03ab95ffe42ea76fc174f0e6caa22f770aa5d08d
sergiu.corches <corchessergiu194@gmail.com>
2024-01-22 18:45:27 +0200

    initial commit

---
+++ QProject/contracts/QERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract QERC20 is ERC20Permit {
+
+    address public immutable owner;
+
+    constructor() ERC20("Q Token", "Q")
+    ERC20Permit("Q Token") {
+        owner = msg.sender;
+    }
+
+    function mintReward(address account, uint256 amount) external {
+        require(msg.sender == owner, "DBXen: caller is not DBXen contract.");
+        require(super.totalSupply() < 5010000000000000000000000, "DBXen: max supply already minted");
+        _mint(account, amount);
+    }
+}
